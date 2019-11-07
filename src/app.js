e67984324b25342e9e48e978cdf49191ca9c8635
Kujtim Prenku <prenkukujtim.kp@gmail.com>
2019-11-07 21:29:10 +0100

    Setup app for heroku bug fix

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const geocode = require('./utils/geocode.js');
 const forecast = require('./utils/forecast.js');
 
 const app = express();
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 
 // Define paths for express config.
 const publicDirectoryPath = path.join(__dirname, '../public');
